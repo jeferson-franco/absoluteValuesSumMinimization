71dc9f9b7b22a824131dce5998b46e1add708cef
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:49:14 -0300

    update absoluteValuesSumMinimization.js

--- src/absoluteValuesSumMinimization.js
+++ src/absoluteValuesSumMinimization.js
@@ -20,3 +20,19 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test absoluteValuesSumMinimization
+
+// alternative solution
+// function solution(A) {
+//     return A[Math.ceil(A.length / 2) - 1];
+// }
+
+// alternative solution
+// function solution(a) {
+//     return a[Math.floor((a.length - 1) / 2)];
+// }
+
+// alternative solution
+// const solution = (a) => {
+//     let distances = a.map((e) => a.reduce((acc, cur) => acc + Math.abs(cur - e), 0));
+//     return a[distances.indexOf(Math.min(...distances))];
+// };
